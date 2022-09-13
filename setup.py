5fb61ca0b09c1dca5ff9e041d6625656137e87a0
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-13 16:41:07 +0300

    chore: add setup.py and setup.cfg configueations

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+
+setup (
+     name="Monolith ecommerce application",
+     author="Emmanuel Langat",
+     author_email="emmanuel@kipchirchirlangat.com",
+     version="0.0.0",
+     packages=find_packages()
+)
