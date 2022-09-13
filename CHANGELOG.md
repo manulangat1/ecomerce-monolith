8c6eee3bcf0d89bf324c2c198aacfa6fa42ed561
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-13 16:48:34 +0300

    Bump version: 0.0.0 → 0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 [Unreleased]
 ------------
 
+[v0.0.1] - 2022-09-13
+------------------
+
 
 <!--
 ### Added
--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup (
      name="Monolith ecommerce application",
      author="Emmanuel Langat",
      author_email="emmanuel@kipchirchirlangat.com",
-     version="0.0.0",
+     version="0.0.1",
+     __version__="0.0.0",
      packages=find_packages()
 )
-__version__ = "0.0.0"
+__version__="0.0.0"
+version= "0.0.0"
